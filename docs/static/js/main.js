6c170c61bd66b36cab7cf84136ac1e674b086f26
iwasakishuto <cabernet.rock@gmail.com>
2021-09-27 02:41:46 +0900

    :baby_chick: Tried to find video element in iframe tag.

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -4,10 +4,11 @@
  * @License MIT license <https://github.com/akari-doichan/embuddyChapterDiviser/blob/main/LICENSE>
  */
 
+var target_video, google_drive_video, google_drive_inner_iframe;
 const video_url_input = document.querySelector("input#video-url");
 const upload_video_btn = document.querySelector("input#upload-video");
 const uploaded_video = document.querySelector("video#uploaded-video");
-const uploaded_video_src = uploaded_video.querySelector("source");
+const google_drive_iframe = document.querySelector("iframe#google-drive");
 const video_start_stop_btn = document.querySelector("button#video-start-stop");
 const video_skip_10s = document.querySelector("button#video-skip-10s");
 const video_back_10s = document.querySelector("button#video-back-10s");
@@ -20,9 +21,12 @@ const video_max_position = document.querySelector("div#video-max-position");
 const video_info = document.querySelector("tbody#video-info");
 
 const startVideoTimer = (url) => {
+  target_video.addEventListener("timeupdate", function () {
+    video_curt_position.textContent = target_video.currentTime.toFixed(1);
+  });
   var updateVideoStatesTimer = setInterval(function () {
-    if (uploaded_video.readyState > 0) {
-      let duration = uploaded_video.duration.toString();
+    if (target_video.readyState > 0) {
+      let duration = target_video.duration.toString();
       video_position_control.max = duration;
       video_max_position.innerHTML = date_formate(duration).slice(0, -3);
       clearInterval(updateVideoStatesTimer);
@@ -38,6 +42,9 @@ upload_video_btn.addEventListener(
     var URL = URL || webkitURL;
     var file = event.target.files[0];
     uploaded_video.src = URL.createObjectURL(file);
+    target_video = uploaded_video;
+    uploaded_video.style.display = "inline";
+    google_drive_iframe.style.display = "none";
     startVideoTimer();
   },
   false
@@ -47,8 +54,24 @@ upload_video_btn.addEventListener(
 video_url_input.addEventListener(
   "change",
   function (event) {
-    uploaded_video_src.src = `https://drive.google.com/uc?export=download&id=${video_url_input.value}`;
-    startVideoTimer();
+    google_drive_iframe.src = `https://drive.google.com/file/d/${video_url_input.value}/preview`;
+    google_drive_iframe.style.display = "inline";
+    uploaded_video.style.display = "none";
+    var start_find_google_drive_video = setInterval(function () {
+      google_drive_inner_iframe = google_drive_iframe.contentWindow.document.querySelector(
+        "iframe#drive-viewer-video-player-object-0"
+      );
+      if (google_drive_inner_iframe != null) {
+        google_drive_video = google_drive_inner_iframe.contentWindow.document.querySelector(
+          "#movie_player > div.html5-video-container > video"
+        );
+        if (google_drive_video != null) {
+          target_video = google_drive_video;
+          startTimer();
+          clearInterval(start_find_google_drive_video);
+        }
+      }
+    }, 500);
   },
   false
 );
@@ -69,33 +92,30 @@ const date_formate = function (ds) {
 var playtimer = null;
 const startTimer = function () {
   playtimer = setInterval(function () {
-    video_position_control.value = uploaded_video.currentTime.toFixed(1);
+    video_position_control.value = target_video.currentTime.toFixed(1);
   }, 100);
 };
 const stopTimer = function () {
   clearInterval(playtimer);
 };
-uploaded_video.addEventListener("timeupdate", function () {
-  video_curt_position.textContent = uploaded_video.currentTime.toFixed(1);
-});
 video_skip_10s.addEventListener("click", function () {
-  uploaded_video.currentTime = Math.min(
-    uploaded_video.currentTime + 10,
-    uploaded_video.duration
+  target_video.currentTime = Math.min(
+    target_video.currentTime + 10,
+    target_video.duration
   );
 });
 video_back_10s.addEventListener("click", function () {
-  uploaded_video.currentTime = Math.max(uploaded_video.currentTime - 10, 0);
+  target_video.currentTime = Math.max(target_video.currentTime - 10, 0);
 });
 video_start_stop_btn.addEventListener("click", function () {
   if (video_start_stop_btn.classList.contains("playing")) {
-    uploaded_video.pause();
+    target_video.pause();
     stopTimer();
     video_start_stop_btn.classList.add("pausing");
     video_start_stop_btn.classList.remove("playing");
     video_start_stop_btn.innerHTML = "Play";
   } else {
-    uploaded_video.play();
+    target_video.play();
     startTimer();
     video_start_stop_btn.classList.add("playing");
     video_start_stop_btn.classList.remove("pausing");
@@ -104,19 +124,19 @@ video_start_stop_btn.addEventListener("click", function () {
 });
 video_speed_control.addEventListener("change", function () {
   let curt_speed = video_speed_control.value;
-  uploaded_video.playbackRate = curt_speed;
+  target_video.playbackRate = curt_speed;
   video_curt_speed.textContent = curt_speed;
 });
 video_position_control.addEventListener("input", function () {
-  uploaded_video.currentTime = video_position_control.value;
+  target_video.currentTime = video_position_control.value;
   stopTimer();
 });
 video_position_control.addEventListener("change", function () {
-  uploaded_video.currentTime = video_position_control.value;
+  target_video.currentTime = video_position_control.value;
   startTimer();
 });
 video_mute_btn.addEventListener("click", function () {
-  uploaded_video.muted = video_mute_btn.checked;
+  target_video.muted = video_mute_btn.checked;
 });
 /* --- Video Controller ---> */
 
@@ -144,7 +164,7 @@ $("#video-info-table").on(
   function (e) {
     if (e.keyCode == 13) {
       // Video is already Uploaded
-      $(this)[0].value = uploaded_video.currentTime.toFixed(0);
+      $(this)[0].value = target_video.currentTime.toFixed(0);
       e.preventDefault();
     }
   }
@@ -164,7 +184,7 @@ $(document).on("click", "#copy2clipboardBtn", function () {
     });
     values.forEach(function (e, i) {
       if (i == values.length - 1) {
-        text += `${e[0]}\t${e[1]}\t${parseInt(uploaded_video.duration) - e[1]}`;
+        text += `${e[0]}\t${e[1]}\t${parseInt(target_video.duration) - e[1]}`;
       } else {
         text += `${e[0]}\t${e[1]}\t${values[i + 1][1] - e[1]}\n`;
       }
